a553661277063582d225db70937a2c004eee3b63
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2024-12-27 14:27:05 +0900

    fix

--- gen1_train_frame.py
+++ gen1_train_frame.py
@@ -11,11 +11,13 @@ data_dir = "/home/ubuntu/gen1/pre_gen1"  # DATA_DIRの値を指定
 input_channels = 3  # 入力チャンネル数
 event_frame_dts = [5, 10, 20, 50, 100]  # 必要に応じて値を追加
 
+gpu_ids_str = ",".join(map(str, gpu_ids)) 
+
 # ループ処理
 for dt in event_frame_dts:
     command = f"""
     python3 RVT/train.py model=rnndet dataset=gen1 dataset.path={data_dir} wandb.project_name=RVT_ssm_gen1_frame_{dt} \
-    wandb.group_name=gen1 +experiment/gen1={mdl_cfg}.yaml hardware.gpus={gpu_ids} \
+    wandb.group_name=gen1 +experiment/gen1={mdl_cfg}.yaml hardware.gpus="[ {gpu_ids_str} ]" \
     batch_size.train={batch_size_per_gpu} batch_size.eval={batch_size_per_gpu} \
     hardware.num_workers.train={train_workers_per_gpu} hardware.num_workers.eval={eval_workers_per_gpu} \
     dataset.ev_repr_name="'event_frame_dt={dt}'" model.backbone.input_channels={input_channels}
