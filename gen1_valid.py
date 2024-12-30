3c3c7234d6de5a032844b207a194179a382f85bf
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2024-12-30 16:22:55 +0900

    update

---
+++ gen1_valid.py
@@ -0,0 +1,25 @@
+import os
+
+# 固定パラメータ
+gpu_ids = 0
+batch_size_per_gpu = 4
+mdl_cfg = "tiny"  # MDL_CFGの値を指定
+data_dir = "/home/metis/Arata_repos/pre_gen1"  # DATA_DIRの値を指定
+ckpt_path = "/home/metis/Arata_repos/RVT_gen1_frame_50/RVT_gen1_frame_50/wjm9eotb/checkpoints/epoch=003-step=106024-val_AP=0.37.ckpt"  # CKPT_PATHの値を指定
+
+input_channels = 3  # 入力チャンネル数
+event_frame_dts = [50]  # 必要に応じて値を追加
+
+
+# ループ処理
+for dt in event_frame_dts:
+    command = f"""
+        python3 RVT/validation.py dataset=gen1 dataset.path={data_dir} checkpoint="'{ckpt_path}'" \
+        +experiment/gen1="{mdl_cfg}.yaml" hardware.gpus={gpu_ids} \
+        batch_size.eval={batch_size_per_gpu} use_test_set=1 \
+        dataset.ev_repr_name="'event_frame_dt={dt}'" model.backbone.input_channels={input_channels} model.postprocess.confidence_threshold=0.001
+        """
+
+
+    print(f"Running command for gen1 event_frame_dt={dt}")
+    os.system(command)  # 実際にコマンドを実行
